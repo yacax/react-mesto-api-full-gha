4c8419140c179c5ea0c6917037ae9b87b386c42d
yacax <yacax@yandex.ru>
2023-06-22 20:22:39 +0300

    provide new port for backend

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,14 +1,5 @@
 const pictureToken = 'kClVgnlqdZgmjLbH6J4KAGnGnGPbdEVH0BEtxF45OUk';
 
-// const baseUrl = 'http://localhost:3001';
-// const baseUrlAuth = 'http://localhost:3001';
-
-// const baseUrl = 'http://51.250.70.104:3001';
-// const baseUrlAuth = 'http://51.250.70.104:3001';
-
-// const baseUrl = 'http://yan.nomoreparties.sbs:3001';
-// const baseUrlAuth = 'http://yan.nomoreparties.sbs:3001';
-
 const baseUrl = 'http://yan.nomoreparties.sbs';
 const baseUrlAuth = 'http://yan.nomoreparties.sbs';
 
