ba0b74a862d74c03ce2f623eace276d2622e80fe
yacax <yacax@yandex.ru>
2023-06-25 18:11:10 +0300

    Fix JWT bug

--- backend/app.js
+++ backend/app.js
@@ -39,6 +39,12 @@ app.use(express.json());
 app.use(requestLogger);
 app.use(cors(corsOptions));
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', validateAuthentication, login);
 app.post('/signup', validateUserBody, createUser);
 
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,4 @@
-const { JWT_SECRET = 'JWT_SECRET' } = process.env;
+const { NODE_ENV, JWT_SECRET = 'JWT_SECRET' } = process.env;
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
@@ -69,7 +69,7 @@ module.exports.login = (req, res, next) => {
 
           const token = jwt.sign(
             { _id: user._id },
-            JWT_SECRET,
+            NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
             { expiresIn: '7d' },
           );
 
